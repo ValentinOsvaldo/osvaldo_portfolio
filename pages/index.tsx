eb0f19740929352d677e25eb676f85abf6c4952e
Osvaldo Valentin <69062379+ValentinOsvaldo@users.noreply.github.com>
2023-03-30 12:29:02 -0600

    hotfix

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
-import { Hero } from '@/components/Hero';
 import Head from 'next/head';
+import { Hero } from '@/components/Hero';
 
 export default function Home() {
   return (
@@ -7,7 +7,7 @@ export default function Home() {
       <Head>
         <title>Osvaldo Valentin</title>
       </Head>
-      <Hero />;
+      <Hero />
     </>
   );
 }
