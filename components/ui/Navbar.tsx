f82fed2572a5044d81e883c1386b2af4085ca347
Osvaldo Valentin <69062379+ValentinOsvaldo@users.noreply.github.com>
2023-04-04 11:04:46 -0600

    add description

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -1,7 +1,12 @@
 import { useScheme } from '@/hooks';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-import { BsLightbulbFill, BsLightbulbOff } from 'react-icons/bs';
+import {
+  BsLightbulbFill,
+  BsLightbulbOff,
+  BsGithub,
+  BsLinkedin,
+} from 'react-icons/bs';
 
 const links = [
   { url: '/projects', text: 'Projects' },
@@ -25,15 +30,30 @@ export const Navbar = () => {
             <Link
               key={page.url}
               href={page.url}
-              className={`text-black dark:text-gray-200 hover:border-b-black font-medium border-b-2 transition-all ${
-                asPath === page.url
-                  ? 'border-b-black dark:text-teal-300 dark:border-b-teal-300'
-                  : 'border-b-transparent'
+              className={`text-gray-800 dark:text-gray-200 hover:border-b-black font-medium transition-all ${
+                asPath === page.url && ' dark:text-teal-300'
               } dark:hover:border-b-teal-300 dark:hover:text-teal-300`}
             >
               {page.text}
             </Link>
           ))}
+
+          <a
+            href="https://github.com/ValentinOsvaldo"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="transition-all text-gray-800 dark:text-gray-300 hover:text-teal-500 dark:hover:text-teal-500"
+          >
+            <BsGithub size={24} />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/valentin-osvaldo/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="transition-all text-gray-800 dark:text-gray-300 hover:text-teal-500 dark:hover:text-teal-500"
+          >
+            <BsLinkedin size={24} />
+          </a>
 
           <button
             className="bg-gray-400 text-white p-2 rounded-full flex items-center justify-center cursor-pointer"
@@ -45,15 +65,6 @@ export const Navbar = () => {
               <BsLightbulbOff size={14} />
             )}
           </button>
-
-          {/* <a
-            href="./osvaldo_valentin_garcia.pdf"
-            target='_blank'
-            rel="noopener noreferrer"
-            className="hidden px-4 py-2 bg-teal-400 font-medium text-black rounded-md sm:inline-block w-fit transition-all hover:brightness-90"
-          >
-            Resume
-          </a> */}
         </nav>
       </div>
     </header>
